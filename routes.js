c0128eca53d9b1c2f2017ec81a1bda686427e91a
Leoris <danielaaleoris@outlook.es>
2024-02-28 10:03:12 -0300

    commit 2

---
+++ routes.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+
+// import LoginScreen from './screens/login/login';
+// import HomeScreen from './screens/home/home';
+
+// const Stack = createStackNavigator();
+
+// const Routes = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="LoginScreen" component={LoginScreen} />
+//         <Stack.Screen name="HomeScreen" component={HomeScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+
+// export default Routes;
---
+++ screens/home/home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, KeyboardAvoidingView, TextInput, TouchableOpacity, Text, Image } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
+import  {style} from './style'
+
+
+export default function HomeScreen(){
+    return(
+        <View>
+            <Text>Hi Lorena</Text>
+            <Text>Hi Lorena</Text>
+            <Text>Hi Lorena</Text>
+            <Text>Hi Lorena</Text>
+            <Text>Hi Lorena</Text>
+            <Text>Hi Lorena</Text>
+        </View>
+    )
+}

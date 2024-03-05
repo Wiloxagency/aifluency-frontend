bad13732f150aa7194f3b40ee9328b4eedf05054
sergio_manica <serginhomanica@hotmail.com>
2024-03-05 16:05:02 -0300

    Add: New screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import LoginScreen from "./src/screens/login/login";
 import HomeScreen from "./src/screens/home/home";
+import ConversationalChat from "./src/screens/conversational-chat/conversational-chat";
 
 
 const App = () => {
